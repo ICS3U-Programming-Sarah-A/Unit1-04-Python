9d21d71522d9b09ed9b1b2eb41b27c332da170ea
Ubuntu <ubuntu@ip-172-31-55-166.ec2.internal>
2022-02-22 23:27:12 +0000

    Created are_per_rect.py, as well as, basic math.

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Sarah
+# Created on: Feb. 22, 2022
+# This program calculates and displays the area and perimeter of a 
+# rectangle with length 10cm and width 8cm.
+
+def main():
+  print ("For a rectangle that has dimensions")
+  print ("length 10cm and width 8cm:")
+  print ("")
+  print ("Area = {} cm^2". format (10*8))
+  print ("Perimeter = {} cm". format (2*(10+8)))
+
+
+if __name__ == "__main__":
+  main()
